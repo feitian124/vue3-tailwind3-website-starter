05a76bfd59cd09d8bdcaf7c327741a333baa2c1e
feitian124 <feitian124@gmail.com>
2022-03-06 08:22:53 +0800

    make counter store itself increase

--- src/stores/counter.ts
+++ src/stores/counter.ts
@@ -1,19 +1,23 @@
-import { acceptHMRUpdate, defineStore } from 'pinia'
-import { ref } from 'vue'
+import { acceptHMRUpdate, defineStore } from "pinia";
+import { ref } from "vue";
+import { useIntervalFn } from "@vueuse/core";
 
-export const useCounterStore = defineStore('user', () => {
+export const useCounterStore = defineStore("counter", () => {
+  const count = ref(0);
 
-  const count = ref(0)
-  
   function increment() {
-    count.value++
+    count.value++;
   }
+
+  useIntervalFn(() => {
+    increment();
+  }, 1000);
 
   return {
     count,
     increment,
-  }
-})
+  };
+});
 
 if (import.meta.hot)
-  import.meta.hot.accept(acceptHMRUpdate(useCounterStore, import.meta.hot))
+  import.meta.hot.accept(acceptHMRUpdate(useCounterStore, import.meta.hot));
